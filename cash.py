cdfb7315fb730b08a89795c69486d6c587f7dbaa
Pedro A. Moreno <pedabraham@gmail.com>
2021-03-04 17:39:48 -0600

    Add change calculator

---
+++ cash.py
@@ -0,0 +1,23 @@
+#get input
+change = -1
+while change < 0:
+    try:
+        change = float(input())
+    except:
+        pass
+#convert to cents
+change *= 100
+
+coin_values = [25,10,5,1]
+
+#get number of coins
+total_coins = 0
+for value in coin_values:
+    coins_per_value = change//value
+    if coins_per_value:
+        total_coins += coins_per_value
+        change = change % value
+        if change == 0:
+            break
+
+print(int(total_coins))
